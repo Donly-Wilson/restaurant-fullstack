57ee4cdac2bd4b27ed102a368b0455c7c0e00483
Donly-Wilson <donly6@hotmail.com>
2020-11-17 20:22:36 -0400

    build waitlist section

--- resources/views/pages/waitlist.blade.php
+++ resources/views/pages/waitlist.blade.php
@@ -1,7 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="">
-    <h1>Waitlist Page</h1>
+<div id="waitlist-page">
+    <div class="content-box">
+        <div class="row">
+            <div class="col-md-6">
+                <h1>Get On The list</h1>
+                <form>
+                    <div class="form-group">
+                      <label for="emailinput">Email address</label>
+                      <input type="email" class="form-control" name="email" id="emailinput" placeholder="name@example.com">
+                    </div>
+                    <div class="form-group">
+                        <label for="phoneinput">Phone no.</label>
+                        <input type="text" class="form-control" name="phone" id="phoneinput" placeholder="758-321-7801">
+                      </div>
+                    <div class="form-group">
+                      <label for="guestsinput">How Many Guest?</label>
+                      <select class="form-control" name="guests" id="guestsinput">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                      </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="timeinput">What Time?</label>
+                        <select class="form-control" name="time" id="timeinput">
+                          <option value="6">6:00 PM</option>
+                          <option value="7">7:00 PM</option>
+                          <option value="8">8:00 PM</option>
+                          <option value="9">9:00 PM</option>
+                          <option value="10">10:00 PM</option>
+                        </select>
+                      </div>
+                      <div class="form-group">
+                        <button type="submit" class="btn btn-primary mb-2">Confirm</button>
+                      </div>
+                  </form>
+            </div>
+            <div class="col-md-6">
+                <p>Please Note: This is not a reservation. You will be added to the current wait list and get notified to have a resavation schedule.You may have a short wait once you have arrive while we prepare your table.</p>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
