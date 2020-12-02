0a9c5bb3ea5a36195edd341dafb40499502bf98e
Donly Wilson <donly6@hotmail.com>
2020-12-02 19:17:48 -0400

    Add recent resavation table with data to dashboard

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -57,11 +57,15 @@ class AdminController extends Controller
             where role_id = 2
         '));
 
+        //Selects all "guest_total" in reservation table more than 0, return a limit of 5 and place it newest to oldest(desc)
+        $latestReservations = Reservation::where('guest_total', '>', 0)->limit(5)->orderBy('created_at', 'desc')->get();
+
         return view('admin/dashboard', [
             'estimated_income_last_30' => "$" . $estimated_income_last_30, //"[0]->total" already reAssign above
             'total_customers_last_30' => $total_customers_last_30[0]->total,
             'total_reservation_last_30' => $total_reservation_last_30[0]->total,
-            'total_employees' => $total_employees[0]->total
+            'total_employees' => $total_employees[0]->total,
+            'latestReservations' => $latestReservations //passed down latest reservations variable to route
         ]);
     }
 }
