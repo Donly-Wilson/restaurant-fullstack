bd35d6c22dc53bbb50fbad09db095c161db13895
Donly Wilson <donly6@hotmail.com>
2020-12-02 12:29:18 -0400

    calc total customers,reservation and employee for dashboard

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -19,19 +19,49 @@ class AdminController extends Controller
 
     public function dashboard()
     {
-        //This calculates the sum of guest_total column for last 30 days and returns it in an array
+        //This calculates the sum of "guest_total" column form reservations table for last 30 days and multiply it by $27(random num for average meal price) returns it in an array
         $estimated_income_last_30 = DB::select(DB::raw('
             SELECT
-                sum(guest_total) as total
+                (sum(guest_total)*27) as total
                 FROM restaurant.reservations
                 WHERE created_at BETWEEN CURDATE()-INTERVAL 30 DAY AND CURDATE()
         '));
         // return dd($estimated_income_last_30[0]->total);
-
         $estimated_income_last_30 = $estimated_income_last_30[0]->total;
 
+
+        //This calculates the sum of "guest_total" column form reservations table last for 30 days and returns it in an array
+        $total_customers_last_30 = DB::select(DB::raw('
+        SELECT
+            sum(guest_total) as total
+            FROM restaurant.reservations
+            WHERE created_at BETWEEN CURDATE()-INTERVAL 30 DAY AND CURDATE()
+        '));
+
+
+        //This calculates a count of all rows in reservations table and returns it in an array
+        $total_reservation_last_30 = DB::select(DB::raw('
+            SELECT
+                count(*) as total
+                FROM restaurant.reservations
+                WHERE created_at BETWEEN CURDATE()-INTERVAL 30 DAY AND CURDATE()
+        '));
+
+
+        //This calculates a count of all rows for users with 2(employee) as role_id and returns it in an array
+        $total_employees = DB::select(DB::raw('
+            select count(*) as total
+            from restaurant.users
+            inner join role_user on role_user.user_id=users.id
+            inner join roles on roles.id=role_user.role_id	
+            where role_id = 2
+        '));
+
         return view('admin/dashboard', [
-            'estimated_income_last_30' => "$" . $estimated_income_last_30
+            'estimated_income_last_30' => "$" . $estimated_income_last_30, //"[0]->total" already reAssign above
+            'total_customers_last_30' => $total_customers_last_30[0]->total,
+            'total_reservation_last_30' => $total_reservation_last_30[0]->total,
+            'total_employees' => $total_employees[0]->total
         ]);
     }
 }
