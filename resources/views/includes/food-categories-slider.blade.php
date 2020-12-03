b968efe136e7993ef458ba9e1484b527cf73a47e
Donly Wilson <donly6@hotmail.com>
2020-12-03 15:17:31 -0400

    working slider for tablet

--- resources/views/includes/food-categories-slider.blade.php
+++ resources/views/includes/food-categories-slider.blade.php
@@ -41,7 +41,7 @@
       $('.sliding-system').css('transform', 'translate3d(-0%,0,0');
     })
 
-    //Function to ensure slider is mobile ready
+    //Function to ensure slider is mobile ready //Desktop view
     function desktopSlider() {
       if (a.matches) {
         maxSlides = Math.ceil((slides * 20) / 100);
@@ -70,9 +70,33 @@
     desktopSlider(a)
     a.addListener(desktopSlider)
 
+    //Tablet view
     function tabletSlider() {
       if (b.matches){
-        
+        maxSlides = Math.ceil((slides * 50) / 100)
+        moveTotal = 100
+
+        //right btn
+        $('.right-btn').on('click', () => {
+          console.log('right btn click')
+          if(currentSlide < ((maxSlides * 100) - 100)){
+            currentSlide += 50;
+            $('.sliding-system').css('transform', `translate3d(-${currentSlide}%, 0, 0)`);
+          }
+        })
+        //left btn
+        $('.left-btn').on('click', () => {
+          console.log('left btn click')
+          if(currentSlide != 0){
+            currentSlide -= 50;
+            $('.sliding-system').css('transform', `translate3d(-${currentSlide}%, 0, 0)`);
+          }
+        })
       }
     }
+
+    var b = window.matchMedia("(min-width:768px) and (max-width:997px)")
+    console.log(b);
+    tabletSlider(b)
+    b.addListener(tabletSlider)
   </script>
