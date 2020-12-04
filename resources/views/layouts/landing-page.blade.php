49660ab7b94133007d2bbd869ffa52eab455d566
Donly Wilson <donly6@hotmail.com>
2020-12-04 07:00:27 -0400

    creating hamburger menu for side-menu tablet/mobile

--- resources/views/layouts/landing-page.blade.php
+++ resources/views/layouts/landing-page.blade.php
@@ -3,8 +3,42 @@
     <head>
         <!-- inputs meta-tags from includes folder -->
         @include('includes.metaTags')
+        <style>
+            nav{
+                display: flex;
+                justify-content: space-between
+            }
+            .navbar-toggler{
+                background-color:green;
+            }
+            #app-layout .side-menu{
+                padding: 30px 50px 0;
+                position:fixed;
+                top:0;
+                transition: all 0.3s ease-in-out;
+                z-index: 1;
+            }
+            #app-layout .side-menu .logo{
+                margin-bottom:40px;
+            }
+            #app-layout .side-menu .menu{
+                margin-bottom:40px;
+            }
+            #app-layout .side-menu .social-icons{
+                margin-bottom:10px;
+            }
+            #app-layout .side-menu .location{
+                position: static;
+            }
+        </style>
     </head>
     <body>
+        <nav>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <p>Ballie's Burger</p>
+        </nav>
         <section id="app-layout">
             <!-- inputs welcome-menu from includes folder as side-menu-->
             @include('includes.side-menu')
@@ -19,3 +53,11 @@
         
     </body>
 </html>
+
+<script>
+    sideMenuBtn = $('.navbar-toggler')
+    sideMenuBtn.on('click', function() {
+        $('.side-menu').css('transform', `translate3d(0%, 0, 0)`)
+        // console.log("click");
+    })
+</script>
