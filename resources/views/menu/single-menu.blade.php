ec39db36d10c275b0a75213ff41a633d9d760bfb
Donly Wilson <donly6@hotmail.com>
2020-12-08 15:19:49 -0400

    add quick categories to single-menu blade

--- resources/views/menu/single-menu.blade.php
+++ resources/views/menu/single-menu.blade.php
@@ -8,6 +8,23 @@
 <div id="single-menu-page">
     <div class="row">
         <div class="col-sm-12 col-md-10 offset-md-1 col-ld-8 offset-ld-2">
+            <div class="quickCategories">
+                <a href="" class="backBtn">Starter</a>
+                <a class="backBtn">Burger</a>
+                <a class="backBtn">Drinks</a>
+                <a class="backBtn">Entrees</a>
+                <a class="backBtn">Entrees</a>
+                <a class="backBtn">Entrees</a>
+                <a class="backBtn">Entrees</a>
+                <a class="backBtn">Entrees</a>
+                <a class="backBtn">Entrees</a>
+                <a class="backBtn">Entrees</a>
+                <a class="backBtn">Entrees</a>
+                <a class="backBtn">Entrees</a>
+                <a class="backBtn">Entrees</a>
+                <a class="backBtn">Entrees</a>
+
+            </div>
             <div class="content-box">
                 <div class="row">
                     <div class="col-md-12">
