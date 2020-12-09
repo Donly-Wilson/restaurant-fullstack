3eedc3c221e28224cc226b5820571bf97cca841f
Donly Wilson <donly6@hotmail.com>
2020-12-09 15:58:40 -0400

    Building footer

--- resources/views/layouts/landing-page.blade.php
+++ resources/views/layouts/landing-page.blade.php
@@ -5,6 +5,7 @@
         @include('includes.metaTags')
         <style>
             footer{
+                
                 margin-top: -13px;
                 color:white;
             }
@@ -17,8 +18,12 @@
             }
             .footer-contents{
                 background:#323231;
+                padding: 40px;
                 /* height: 300px; */
                 margin-top: -1px
+            }
+            .footer-extras{
+                background:#323231;
             }
         </style>
     </head>
@@ -41,17 +46,23 @@
             <div class="footer-image">
                 <img src="/img/footer.svg" alt="">
             </div>
-            <div class="footer-contents row">
-                <div class="footer-info col-md-6">
-                    <p>Copyright © 2020 Big Chef Steakhouse. All rights reserved.</p>
-                    <p>COVID-19 Opening Approval</p>
+            <div class="footer-contents row no-gutters">
+                <div class="footer-info col-md-7">
+                    <h2>Bailie's Burger</h2>
+                    <ul class="links">
+                        <li><a href="/menu">Food</a></li>
+                        <li><a href="/reservations">Reservations</a></li>
+                        <li><a href="/offers">Offers</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                        <li><a href="/about">About Us</a></li>
+                      </ul>
                 </div>
-                <div class="map col-md-6">
+                <div class="map col-md-5">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3870.0990527598574!2d-60.9581723851427!3d14.071327793599698!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c4068278000428b%3A0xbe25441b9a1fea41!2sTriangle%20PUB%2C%20St%20Lucia!5e0!3m2!1sen!2s!4v1606865834386!5m2!1sen!2s" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                 </div>
             </div>
-            <div class="row">
-                <p>Copyright © 2020 Big Chef Steakhouse. All rights reserved.</p>
+            <div class="footer-extras">
+                <p>Copyright © 2020 Bailie's Burger. All rights reserved.</p>
                     <p>COVID-19 Opening Approval</p>
             </div>
         </footer>
