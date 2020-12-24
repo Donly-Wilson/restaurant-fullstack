2a96619412c8a069c07b66ff22659a191809ccb0
Donly Wilson <donly6@hotmail.com>
2020-12-24 19:29:50 -0400

    added Procfile for heroku deployment

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -138,10 +138,11 @@
 
                                 @if (Route::has('login'))
                                     {{-- <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block"> --}}
-                                        @auth
+                                            @auth
                                             {{-- This form is a post request that logout when clicked --}}
                                             <form method="POST" action="{{ route('logout') }}">
                                                 @csrf
+                                                {{-- <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a> --}}
                                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                                                 onclick="event.preventDefault();
                                                                             this.closest('form').submit();">
