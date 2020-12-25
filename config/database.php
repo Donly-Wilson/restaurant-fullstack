8b0cd58f38cec45a91738a5ca8bcfa6bd1578021
Donly Wilson <donly6@hotmail.com>
2020-12-24 22:48:14 -0400

    better string type

--- config/database.php
+++ config/database.php
@@ -53,7 +53,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
-            'collation' => 'utf8general_ci',
+            'collation' => 'utf8_general_ci',
             // 'charset' => 'utf8mb4',
             // 'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
