464e7a61b89186b0359c3ea5a66844ce95b60730
Donly Wilson <donly6@hotmail.com>
2020-12-29 20:08:07 -0400

    test graph again

--- app/Http/Controllers/DashboardApiController.php
+++ app/Http/Controllers/DashboardApiController.php
@@ -41,9 +41,9 @@ class DashboardApiController extends Controller
         return $estimated_income_daily_data = DB::select(DB::raw('
             SELECT 
                 DATE_FORMAT(created_at,"%Y-%m-%d") as x,
-                (sum(guest_total)*27)as y
-                FROM reservations
-                group by x desc;
+                (sum(guest_total)*27) as y
+            FROM reservations
+            group by x desc;
         '));
     }
 }
