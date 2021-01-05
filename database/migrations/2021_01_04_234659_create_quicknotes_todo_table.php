13ec7f6a260ba7bfcfe523b2374674063f222953
Donly Wilson <donly6@hotmail.com>
2021-01-04 20:53:36 -0400

    set up Todo migration/controller/model

---
+++ database/migrations/2021_01_04_234659_create_quicknotes_todo_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuicknotesTodoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quicknotes_todo', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quicknotes_todo');
+    }
+}
