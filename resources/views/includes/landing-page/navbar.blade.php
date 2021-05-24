0371f91b30ed37836c810c70b3ca883d85010f67
Donly Wilson <donly6@hotmail.com>
2021-05-23 22:13:37 -0400

    Complete planned fixes

--- resources/views/includes/landing-page/navbar.blade.php
+++ resources/views/includes/landing-page/navbar.blade.php
@@ -1,5 +1,8 @@
 <nav id="headerNav">
-    <p>Ballie's Burger</p>
+    <a class="headerNav-logo col-md-12" href="/">
+        <img src="./img/restaurant-logo.png" alt="">
+        <p>Ballie's Burger</p>
+    </a>
     <button class="navbar-toggler" type="button">    
         <div class="hamburger-icon">
             <span></span>
